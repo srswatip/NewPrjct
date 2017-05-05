6a8e19e35372d02b2ea6ff4e4dba5851167a2262
srswatip <rezacakep1@gmail.com>
2017-05-04 20:31:01 -0700

    initial commit

---
+++ app/src/main/java/co/id/ochann/Fruit.java
@@ -0,0 +1,6 @@
+package co.id.ochann
+        import android.content.intent;
+import android.support.v7.app.AppCompatAct
+        import android.os.Bundle;
+                import android.view.View;
+import android.widget.button
